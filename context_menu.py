d5d0d582cc658557bff41764a8c8896c97a517eb
artemisxrpg <74358884+artemisxrpg@users.noreply.github.com>
2025-08-29 15:25:10 -0700

    Update context_menu.py

--- context_menu.py
+++ context_menu.py
@@ -19,9 +19,9 @@ def main():
         'kodi_type': kodi_type
     }
     window = Window(10000)
-    while window.getProperty('plexkodiconnect.command'):
+    while window.getProperty('plexkodiconnect2.command'):
         sleep(20)
-    window.setProperty('plexkodiconnect.command',
+    window.setProperty('plexkodiconnect2.command',
                        'CONTEXT_menu?%s' % urlencode(args))
 
 
